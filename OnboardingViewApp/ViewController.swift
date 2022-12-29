2679dc43ff2d4628c61b28a0869a391daf67c7d2
LeeChungWoo <ios.lllccww@gmail.com>
2022-12-29 23:20:32 +0900

    [UPDATE] ViewController 실행하면 PageViewController 띄우게 변경

--- OnboardingViewApp/ViewController.swift
+++ OnboardingViewApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+ 
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let pageVC = OnBoardingPageViewController()
+        self.present(pageVC, animated: true)
+    }
+    
+    
 }
 
